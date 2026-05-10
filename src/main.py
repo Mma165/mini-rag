065ac646499281542d8a61fa4593abc4c6446838
Mma165 <mahmoudali1652004@gmail.com>
2026-05-11 00:30:00 +0300

    Fix formatting in main.py by adding whitespace for improved readability

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from routes import base_router, data_router
 from helpers import get_settings
 from motor.motor_asyncio import AsyncIOMotorClient 
+
+
 app = FastAPI() 
 @app.on_event("startup")
 async def startup_db_client():
@@ -11,7 +13,7 @@ async def startup_db_client():
 @app.on_event("shutdown")
 async def shutdown_db_client():
     app.mongodb_connection.close() 
+    
 
 app.include_router(base_router)
 app.include_router(data_router)
-
