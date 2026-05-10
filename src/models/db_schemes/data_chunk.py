2c43a356e36b917117979f80e00a2a9d62b9b267
Mma165 <mahmoudali1652004@gmail.com>
2026-05-11 02:35:09 +0300

    Refactor ProjectModel and DataChunk to unify field definitions; replace _id with id in ProjectModel and enforce stricter typing in DataChunk

--- src/models/db_schemes/data_chunk.py
+++ src/models/db_schemes/data_chunk.py
@@ -3,10 +3,10 @@ from typing import Optional
 from bson.objectid import ObjectId
 class DataChunk(BaseModel):
     id : Optional[ObjectId]= Field(None,alias="_id")
-    chunk_txt: Optional[str]=Field(..., min_length=1)
-    chunk_metadata: Optional[dict]
-    chunk_order: Optional[int] =Field(..., ge=0)
-    chunk_project_id: Optional[ObjectId] = Field(..., min_length=1)
+    chunk_txt: str=Field(..., min_length=1)
+    chunk_metadata: dict
+    chunk_order: int =Field(..., gt=0)
+    chunk_project_id: ObjectId = Field(...)
 
     class Config:
         arbitrary_types_allowed = True
